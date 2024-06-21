c31bfdf95ff8150a72b6477f7f03298e028084e1
SakibSibly <sibly@duck.com>
2024-06-21 22:47:51 +0600

    files added

--- system_prog/signals.c
+++ system_prog/signals.c
@@ -1,7 +1,24 @@
 #include <signal.h>
 #include <stdio.h>
-    #include <unistd.h>
+#include <unistd.h>
 
+/*****************AMBIGUOUS********************/
+
+// struct sigaction {
+//     void* sa_handler;
+//     int sa_flag;
+// };
+
+// void wakeup() {}
+
+// unsigned int sleep(unsigned int timer) {
+//     struct sigaction action;
+//     action.sa_handler = wakeup;
+//     action.sa_flag = 0;
+//     sigemptyset()
+// }
+
+/***********************************************/
 void catch_sig(int sig_num) {
     signal(sig_num, catch_sig);
     printf("catch_sig: %d\n", sig_num);
